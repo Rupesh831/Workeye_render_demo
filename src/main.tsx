472f54311cfe58c13119845f10e506c2a2264ee5
Rupesh831 <rupesh.m@rajlaxmiworld.com>
2026-01-21 18:25:28 +0530

    Fix: Add BrowserRouter to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
+// UPDATED: 2026-01-21 18:23 IST
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
